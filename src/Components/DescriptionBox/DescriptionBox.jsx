6550c41220108fb3e3fefb5dd708215490f24579
yogendrachowdary <yogendrachowdary101@gmail.com>
2023-11-28 01:39:57 +0530

    day 2 done

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className='descriptionbox-navigator'>
+      <div className='descriptionbox-nav-box'>Description</div>
+      <div className='descriptionbox-nav-box fade'>Reviews(122)</div>
+      </div>
+      <div className='descriptionbox-description'>
+        <p>
+        Browse our carefully curated selection of fashion-forward clothing, accessories, and lifestyle products designed to elevate your look effortlessly. With a user-friendly shopping experience, swift delivery, and responsive customer service, we bring you unparalleled fashion for every occasion. Explore sustainable choices, exclusive deals, and follow us on social media for daily style inspiration. Subscribe to our newsletter for early access to sales. At Shopper, we're not just selling products; we're crafting an experience that empowers you to express your unique style. Start shopping now and let your wardrobe speak volumes!  
+        </p>
+        <p>Indulge in the art of fashion at Shopper, where curated collections of chic clothing and accessories await. Elevate your style effortlessly with our diverse range, offering a seamless shopping experience for the modern trendsetter.</p>
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
